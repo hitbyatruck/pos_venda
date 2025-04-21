b950ce72e64bfee77e40aa84a5bd90010ad24113
HBT <hitbyatruck@gmail.com>
2025-04-21 15:52:15 +0100

    Initial commit before cleanup

---
+++ git_commands.sh
@@ -0,0 +1,8 @@
+# Stage all the deleted files
+git add --all
+
+# Commit the changes with a descriptive message
+git commit -m "Project cleanup: removed migration backups and screenshot files"
+
+# Push the changes to your remote branch (optional)
+git push origin navegacao-unificada
